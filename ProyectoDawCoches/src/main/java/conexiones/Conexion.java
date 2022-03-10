ab728f352ce002876fd3036b30386fe00c006cc0
esteban ignacio <alumno.1198529@ies-azarquiel.es>
2022-03-10 11:43:29 +0100

    Creación Servlet, test y Dao

--- ProyectoDawCoches/src/main/java/conexiones/Conexion.java
+++ ProyectoDawCoches/src/main/java/conexiones/Conexion.java
@@ -5,7 +5,6 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
@@ -14,40 +13,45 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import conexiones.Conexion;
 
-
 public class Conexion {
 	// Definir la ruta de la base de datos
-		private String dbUrl = "jdbc:mysql://localhost:3306/Coches?serverTimezone=Europe/Rome";
-		// Definir el nombre de usuario de la base de datos
-		private String dbUser = "Coches";
-		// Definir la contraseña de la base de datos
-		private String dbPassword = "Coches";
-		// Definir controlador de carga
-		private String jdbcName = "com.mysql.jdbc.Driver";
+	private String dbUrl = "jdbc:mysql://localhost:3306/Coches?serverTimezone=Europe/Rome";
+	// Definir el nombre de usuario de la base de datos
+	private String dbUser = "Coches";
+	// Definir la contraseña de la base de datos
+	private String dbPassword = "Coches";
+	// Definir controlador de carga
+	private String jdbcName = "com.mysql.jdbc.Driver";
+
 	public Conexion() {
 		// TODO Auto-generated constructor stub
 	}
 
-	 // Conéctate a la base de datos
-		public Connection getConn() {
-			Connection conn = null;
-			try {
-				Class.forName(jdbcName);
-			} catch (Exception e) {
-				 System.out.println ("¡Error al cargar el controlador!");
-			}
-			try {
-				conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-			} catch (SQLException ex) {
-				 System.out.println ("¡Error al conectarse a la base de datos!");
-			}
-			return conn;
+	// Conéctate a la base de datos
+	public Connection getConn() throws SQLException {
+		Connection conn = null;
+		try {
+			Class.forName(jdbcName);
+		} catch (Exception e) {
+			System.out.println("¡Error al cargar el controlador!");
 		}
-	 
-		 // prueba
-		public static void main(String[] args) {
-			// System.out.println(new DBHelper().getConn());
-			 System.out.println ("¡Conectado exitosamente a la base de datos!");
+		try {
+			conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+		} catch (SQLException ex) {
+			System.out.println("¡Error al conectarse a la base de datos!");
 		}
+		return conn;
+	}
+
+	// prueba
+	public static void main(String[] args) {
+		 try {
+			System.out.println(new Conexion().getConn());
+		
+
+		 } catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
